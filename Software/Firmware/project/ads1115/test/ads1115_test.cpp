446569db0fdddbcbb38087b052352aa3c9061d68
Jamie Milsom <jamieamilsom@gmail.com>
2024-03-03 23:32:39 +0000

    HEAR-13 Added Google Test framework and initial test files.
    * Added gtest dependency and configuration to CMakeLists.txt.
    * Created basic test files for heartguard, ads1115, and max30102.

---
+++ Software/Firmware/project/ads1115/test/ads1115_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+TEST(Ads1115Test, BasicStructure) {
+  // This test doesn't actually interact with any functionality,
+  // but it verifies that your header files can be included 
+  // and compilation works as expected.
+  ASSERT_TRUE(true);  // Just a placeholder assertion
+}
